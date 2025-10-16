2022099d06f88cb24928807096603c047b62e672
Zuzana Kovacsova <suz.kovacsova@gmail.com>
2025-10-16 19:49:24 +0100

    Agent chatbot app

---
+++ streamlit-chatbot-app/app.py
@@ -0,0 +1,77 @@
+import logging
+import os
+import streamlit as st
+from databricks.sdk import WorkspaceClient
+
+w = WorkspaceClient()
+client = w.serving_endpoints.get_open_ai_client()
+
+# Set up logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+# Ensure environment variable is set correctly
+SERVING_ENDPOINT = os.getenv('SERVING_ENDPOINT')
+assert SERVING_ENDPOINT, \
+    ("Unable to determine serving endpoint to use for chatbot app. If developing locally, "
+     "set the SERVING_ENDPOINT environment variable to the name of your serving endpoint. If "
+     "deploying to a Databricks app, include a serving endpoint resource named "
+     "'serving_endpoint' with CAN_QUERY permissions, as described in "
+     "https://docs.databricks.com/aws/en/generative-ai/agent-framework/chat-app#deploy-the-databricks-app")
+
+def get_user_info():
+    headers = st.context.headers
+    return dict(
+        user_name=headers.get("X-Forwarded-Preferred-Username"),
+        user_email=headers.get("X-Forwarded-Email"),
+        user_id=headers.get("X-Forwarded-User"),
+    )
+
+user_info = get_user_info()
+
+# Streamlit app
+if "visibility" not in st.session_state:
+    st.session_state.visibility = "visible"
+    st.session_state.disabled = False
+
+st.title("🧱 Chatbot App")
+
+
+st.markdown(
+    "ℹ️ This is a simple example of a chatbot querying an agent."
+)
+
+# Initialize chat history
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+# Display chat messages from history on app rerun
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+# Accept user input
+if prompt := st.chat_input("What is up?"):
+    # Add user message to chat history
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    # Display user message in chat message container
+    with st.chat_message("user"):
+        st.markdown(prompt)
+
+    # Display assistant response in chat message container
+    with st.chat_message("assistant"):
+        # Query the Databricks serving endpoint
+        streaming_response = client.responses.create(model=SERVING_ENDPOINT, input=st.session_state.messages, stream=True)
+        resp = []
+        for chunk in streaming_response:
+            if (not hasattr(chunk.item, 'content') or (chunk.item.content is None)):
+                pass
+            else: 
+                resp.append(chunk.item.content[0].text)
+        assistant_response = resp
+        # Non-streaming
+        st.markdown(assistant_response[-1])
+
+
+    # Add assistant response to chat history
+    st.session_state.messages.append({"role": "assistant", "content": assistant_response[-1]})
